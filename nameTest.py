dea6a54280d25a4543de987af3c7aea4c9eb3d4a
Andrea206 <drea567@gmail.com>
2018-06-30 10:59:35 -0700

    My Django Girls app, first commit

---
+++ nameTest.py
@@ -0,0 +1,25 @@
+
+
+def main():
+    printname('Jane')
+    loopName('Andrea!')
+    
+
+def printname(name):
+    if name == 'Andrea':
+        print(name)
+
+    else:
+        print('Not Andrea!')
+
+
+
+def loopName(name):
+    for i in range(0,10):
+        print(name)
+    
+
+
+main()
+    
+    
